830c1a5e8c35d004a9894bd834d50b016343400b
yujiaohe <34307798+yujiaohe@users.noreply.github.com>
2023-03-23 11:39:00 +0800

    uncomments and formatted

--- decorator_function.py
+++ decorator_function.py
@@ -1,57 +1,60 @@
-# # Functions can have inputs/funcitonality/output
-# def add(n1, n2):
-#     return n1 + n2
-#
-#
-# def subtract(n1, n2):
-#     return n1 - n2
-#
-#
-# def multiply(n1, n2):
-#     return n1 * n2
-#
-#
-# def divide(n1, n2):
-#     return n1 / n2
-#
-#
-# # Functions are first-class objects, can be passed around as arguments e.g. int/string/float etc
-# def calculate(calc_function, n1, n2):
-#     return calc_function(n1, n2)
-#
-#
-# result = calculate(add, 2, 3)
-# print(result)
 import time
 
 
-# Nested Functions
-# def outer_function():
-#     print("I'm outer")
-#
-#     def nested_function():
-#         print("I'm inner")
-#
-#     nested_function()
-#
-#
-# outer_function()
+# Functions can have inputs/functionality/output
+def add(n1, n2):
+    return n1 + n2
 
-# # Functions can be returned from other functions
-# def outer_function():
-#     print("I'm outer")
-#
-#     def nested_function():
-#         print("I'm inner")
-#
-#     return nested_function
-#
-#
-# inner_function = outer_function()
-# inner_function()
+
+def subtract(n1, n2):
+    return n1 - n2
+
+
+def multiply(n1, n2):
+    return n1 * n2
+
+
+def divide(n1, n2):
+    return n1 / n2
+
+
+# Functions are first-class objects, can be passed around as arguments e.g. int/string/float etc
+def calculate(calc_function, n1, n2):
+    return calc_function(n1, n2)
+
+
+result = calculate(add, 2, 3)
+print(result)
+
+
+# Nested Functions
+def outer_function():
+    print("I'm outer")
+
+    def nested_function():
+        print("I'm inner")
+
+    nested_function()
+
+
+outer_function()
+
+
+# Functions can be returned from other functions
+def outer_function():
+    print("I'm outer")
+
+    def nested_function():
+        print("I'm inner")
+
+    return nested_function
+
+
+inner_function = outer_function()
+inner_function()
+
 
 # Python Decorator Function
-
 def delay_decorator(function):
     def wrapper_function():
         time.sleep(2)
